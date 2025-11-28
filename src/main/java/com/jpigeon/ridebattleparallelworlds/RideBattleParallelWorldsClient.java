8dab623078e2bc90c05d7bd71adab008c871c095
Junye <d_d5du@163.com>
2025-11-28 23:55:57 +0100

    Initial commit

---
+++ src/main/java/com/jpigeon/ridebattleparallelworlds/RideBattleParallelWorldsClient.java
@@ -0,0 +1,24 @@
+package com.jpigeon.ridebattleparallelworlds;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = RideBattleParallelWorlds.MODID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = RideBattleParallelWorlds.MODID, value = Dist.CLIENT)
+public class RideBattleParallelWorldsClient {
+    public RideBattleParallelWorldsClient(ModContainer container) {
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+
+    }
+}
