a08cfcb882cb8e3c75ddaeb571fffbd5448d8d88
Junye <d_d5du@163.com>
2025-12-13 16:42:42 +0100

    结构改进

--- src/main/java/com/jpigeon/ridebattleparallelworlds/core/datagen/ModItemModelProvider.java
+++ src/main/java/com/jpigeon/ridebattleparallelworlds/core/datagen/ModItemModelProvider.java
@@ -19,45 +19,6 @@ public class ModItemModelProvider extends ItemModelProvider {
     protected void registerModels() {
         // basicItem(ModItems.WORLDS_FRAGMENT.get());
         // 空我
-        basicItem(ModItems.CHOGODAI_ELEMENT.get());
-        basicItem(ModItems.MIGHTY_ELEMENT.get());
-        basicItem(ModItems.DRAGON_ELEMENT.get());
-        basicItem(ModItems.PEGASUS_ELEMENT.get());
-        basicItem(ModItems.TITAN_ELEMENT.get());
-        basicItem(ModItems.RISING_MIGHTY_ELEMENT.get());
-        basicItem(ModItems.RISING_DRAGON_ELEMENT.get());
-        basicItem(ModItems.RISING_PEGASUS_ELEMENT.get());
-        basicItem(ModItems.RISING_TITAN_ELEMENT.get());
-        basicItem(ModItems.AMAZING_MIGHTY_ELEMENT.get());
-        basicItem(ModItems.ULTIMATE_ELEMENT.get());
-
-        // 盔甲
-        basicItem(ModItems.GROWING_HELMET.get());
-        basicItem(ModItems.GROWING_CHESTPLATE.get());
-        basicItem(ModItems.GROWING_BOOTS.get());
-        basicItem(ModItems.MIGHTY_HELMET.get());
-        basicItem(ModItems.MIGHTY_CHESTPLATE.get());
-        basicItem(ModItems.MIGHTY_BOOTS.get());
-        basicItem(ModItems.DRAGON_HELMET.get());
-        basicItem(ModItems.DRAGON_CHESTPLATE.get());
-        basicItem(ModItems.DRAGON_BOOTS.get());
-        basicItem(ModItems.PEGASUS_HELMET.get());
-        basicItem(ModItems.PEGASUS_CHESTPLATE.get());
-        basicItem(ModItems.PEGASUS_BOOTS.get());
-        basicItem(ModItems.TITAN_HELMET.get());
-        basicItem(ModItems.TITAN_CHESTPLATE.get());
-        basicItem(ModItems.TITAN_BOOTS.get());
-        basicItem(ModItems.RISING_MIGHTY_HELMET.get());
-        basicItem(ModItems.RISING_MIGHTY_CHESTPLATE.get());
-        basicItem(ModItems.RISING_MIGHTY_BOOTS.get());
-        basicItem(ModItems.RISING_DRAGON_HELMET.get());
-        basicItem(ModItems.RISING_DRAGON_CHESTPLATE.get());
-        basicItem(ModItems.RISING_DRAGON_BOOTS.get());
-
-        // 驱动器
-        basicItem(ModItems.ARCLE.get());
-
-
     }
 
 
