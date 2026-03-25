a5aea48d9dfd29b016eb148514e76c8281682e6b
Junye <d_d5du@163.com>
2026-03-25 19:13:06 +0100

    added RiderIngot

--- src/main/java/com/jpigeon/ridebattleparallelworlds/core/riders/kuuga/block/KuugaCoffinBlock.java
+++ src/main/java/com/jpigeon/ridebattleparallelworlds/core/riders/kuuga/block/KuugaCoffinBlock.java
@@ -4,25 +4,19 @@ package com.jpigeon.ridebattleparallelworlds.core.riders.kuuga.block;
 import com.jpigeon.ridebattleparallelworlds.core.item.ModItems;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
-import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
-import java.util.Objects;
 
 public class KuugaCoffinBlock extends BaseEntityBlock{
     public static final VoxelShape SHAPE = Block.box(0, 0, -8, 16, 8, 24);
